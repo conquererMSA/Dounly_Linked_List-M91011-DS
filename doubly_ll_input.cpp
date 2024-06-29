7aa4163b3855e5086f7e60ff7f83f8dd0df79886
asus test <msa-99@outlook.com>
2024-06-30 00:25:07 +0600

    take input of a dll

---
+++ doubly_ll_input.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next=NULL;
+    Node* prev=NULL;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+void printLLStraight(Node*head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+void printLLRevers(Node*tail){
+    Node* temp=tail;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp=temp->prev;
+    }
+    cout<<endl;
+}
+void insertAtTail(Node*&head, Node*&tail, int v){
+     Node* newNode=new Node(v);
+     if(head==NULL){
+        tail=NULL;
+        head=newNode;
+        tail=newNode;
+        return;
+     }
+     tail->next=newNode;
+     newNode->prev=tail;
+     tail=newNode;
+}
+int main(){
+    Node*head=NULL;
+    Node*tail=NULL;
+    while(1){
+        int v;
+        cin>>v;
+        if(v==-1) break;
+      insertAtTail(head,tail,v);
+    }
+    printLLStraight(head);
+    printLLRevers(tail);
+    return 0;
+}
